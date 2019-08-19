79c9069dba49fe3a4ea2ec69b172c2850d680aeb
simonppg <simonppg@gmail.com>
2019-08-19 13:28:02 -0700

    console log url

--- src/libs/boot.js
+++ src/libs/boot.js
@@ -2,7 +2,9 @@ module.exports = app => {
     // Create tables and start server
     app.db.sequelize.sync().done(() => {
         app.listen(app.get('port'), () => {
-            console.log('Server on port', app.get('port'))
+            let port = app.get('port')
+            console.log(`Server on port ${port}`)
+            console.log(`http://localhost:${port}`)
         })
     })
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ module.exports = app => {
 
     const User = app.db.models.User;
 
-    app.route('/User')
+    app.route('/users')
         .get((req, res) => {
             User.findAll({})
                 .then(result => {
@@ -19,14 +19,4 @@ module.exports = app => {
                     res.status(412).json({msg: err.message})
                 })
         })
-
-    /*app.get('/user', (req, res) => {
-        User.findAll({})
-        .then(result => {
-            res.json(result)
-        })
-        .catch(err => {
-            res.status(412).json({msg: err.message})
-        })
-    })*/
 }
